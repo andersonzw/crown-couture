9e0a1235356877059834ec101f9b725fe36a04ed
Anderson <84068667+andersonzw@users.noreply.github.com>
2023-10-19 12:11:05 +1100

    created directory and category

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,7 @@
+import Directory from "./components/directory/Directory";
+
 const App = () => {
-  const categories = [
-    {
-      title: "Hats",
-      src: "",
-      id: "1",
-    },
-    {
-      title: "Jackets",
-      src: "",
-      id: "2",
-    },
-    {
-      title: "Sneakers",
-      src: "",
-      id: "3",
-    },
-    {
-      title: "Womens",
-      src: "",
-      id: "4",
-    },
-    {
-      title: "Mens",
-      src: "",
-      id: "5",
-    },
-  ];
-  return (
-    <div className="categories-container">
-      {categories.map((category) => {
-        return (
-          <div className="category-container">
-            <div className="background-image" />
-            <img src={category.src} alt='' />
-            <div className="category-body-container">
-              <h2>{category.title}</h2>
-              <p>Shop Now</p>
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
+  return <Directory />;
 };
 
 export default App;
