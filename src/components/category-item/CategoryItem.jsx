523843bf2ea4213a094d3188f4e1d20e99959b0d
Anderson <84068667+andersonzw@users.noreply.github.com>
2023-11-07 13:12:34 +1100

    added links to directory page

--- src/components/category-item/CategoryItem.jsx
+++ src/components/category-item/CategoryItem.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import "./CategoryItem.scss"
 
 const CategoryItem = ({category}) => {
+  const {title, imageUrl} = category
+  const navigate = useNavigate()
+
+  const onClickHandler = ()=> navigate(`/shop/${title}`)
   return (
-    <div className="category-container">
-    <div className="background-image"  style={{backgroundImage: `url(${category.imageUrl})`}}/>
+    <div onClick = {onClickHandler} className="category-container">
+    <div className="background-image"  style={{backgroundImage: `url(${imageUrl})`}}/>
     <div className="category-body-container">
-      <h2>{category.title}</h2>
+      <h2>{title}</h2>
       <p>Shop Now</p>
     </div>
   </div>
--- src/components/directory/Directory.jsx
+++ src/components/directory/Directory.jsx
@@ -2,34 +2,37 @@ import React from "react";
 import "./Directory.scss";
 import CategoryItem from "../category-item/CategoryItem";
 
+const categories = [
+  {
+    id: 1,
+    title: "hats",
+    imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    
+  },
+  {
+    id: 2,
+    title: "jackets",
+    imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+  },
+  {
+    id: 3,
+    title: "sneakers",
+    imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+  },
+  {
+    id: 4,
+    title: "womens",
+    imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+  },
+  {
+    id: 5,
+    title: "mens",
+    imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+  },
+];
+
+
 const Directory = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
   return (
     <div className="innerWidth paddings directory-container">
       {categories.map((category) => {
@@ -40,4 +43,3 @@ const Directory = () => {
 };
 
 export default Directory;
-
