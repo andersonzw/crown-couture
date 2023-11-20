f4ac127b03b37f41dba1809e451ffaf772a26ae8
Anderson <84068667+andersonzw@users.noreply.github.com>
2023-11-20 16:36:35 +1100

    added stripe into index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,20 @@ import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 import { persistor, store } from "./store/store";
 import ScrollToTop from "./util/SrollToTop";
+import { Elements } from "@stripe/react-stripe-js";
+import { stripePromise } from "./util/stripe/strpe.utils";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-      <BrowserRouter>
-        <ScrollToTop/>
-          <App />
-
-      </BrowserRouter>
+        <BrowserRouter>
+          <ScrollToTop />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
+        </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
